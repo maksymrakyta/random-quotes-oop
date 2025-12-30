ee524a30d21c3bbf8e358adca65a5c63a5bffecd
Maxim Rakyta <maksym.rakyta@gmail.com>
2025-12-30 13:03:21 +0100

    Get random quotes via public API service

--- src/classes/RandomQuote.js
+++ src/classes/RandomQuote.js
@@ -8,6 +8,16 @@ class RandomQuote {
     const { id, text, author } = quotes[randomIndex];
     return new Quote(id, text, author);
   }
+
+  static getRandomQuoteViaAPI() {
+    const url = 'https://quoteslate.vercel.app/api/quotes/random';
+    const options = { headers: { 'Content-Type': 'application/json' } };
+
+    return fetch(url, options)
+      .then((response) => response.json())
+      .then(({ id, quote, author }) => new Quote(id, quote, author))
+      .catch((error) => console.error('Error', error));
+  }
 }
 
 export default RandomQuote;
